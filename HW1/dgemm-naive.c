7ef6ec952aea2e42692491e450d656c4158a3b10
terrawilliams <terraw9@gmail.com>
2020-02-05 15:59:54 -0800

    changed loop order

--- HW1/dgemm-naive.c
+++ HW1/dgemm-naive.c
@@ -22,14 +22,11 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 void square_dgemm (int n, double* A, double* B, double* C)
 {
   /* For each row i of A */
-  for (int i = 0; i < n; ++i)
+  for (int j = 0; j < n; ++j)
     /* For each column j of B */
-    for (int j = 0; j < n; ++j) 
+    for (int k = 0; k < n; ++k)
     {
-      /* Compute C(i,j) */
-      double cij = C[i+j*n];
-      for( int k = 0; k < n; k++ )
-	cij += A[i+k*n] * B[k+j*n];
-      C[i+j*n] = cij;
+      for( int i = 0; i < n; i++ )
+          C[i+j*n] += A[i+k*n] * B[k+j*n];
     }
 }
