60b49a49877189e6d3a6856c5504d6d93426bd51
terrawilliams <terraw9@gmail.com>
2020-02-05 20:51:52 -0800

    first attempt at fma

--- HW1/dgemm-naive.c
+++ HW1/dgemm-naive.c
@@ -30,7 +30,7 @@ void square_dgemm (int n, double* A, double* B, double* C)
     /* For each column j of B */
     for (int k = 0; k < n; ++k)
     {
-      for( int i = 0; i < n; i += 4)
+      for( int i = 0; i < n - 3; i += 4)
       {
           __m256d m1 = _mm256_load_pd(A + i + k * n);
           __m256d m2 = _mm256_broadcast_sd(B + k + j * n);
