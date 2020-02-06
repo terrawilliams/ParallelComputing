0b7788ebaf998a8d5586029969802465a9f06f3f
terrawilliams <terraw9@gmail.com>
2020-02-05 19:55:58 -0800

    first attempt at vectorization

--- HW1/dgemm-naive.c
+++ HW1/dgemm-naive.c
@@ -13,6 +13,7 @@ LDLIBS = -lrt -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_intel_lp64.a $(MKL
 
 */
 
+#include <stdio.h>
 #include <intrin.h>
 
 const char* dgemm_desc = "Naive, three-loop dgemm.";
